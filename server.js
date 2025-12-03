9410f23cd2aff384b3555bc98a95417e31b112f2
Vadim <timur2010rumit0102@gmail.com>
2025-12-03 15:30:41 +0300

    fgh

--- server.js
+++ server.js
@@ -28,9 +28,6 @@ const countdownDuration =  60 * 1000;
 let g_league_users = 0
 let s_league_users = 0
 let b_league_users = 0
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
 app.get('/get_users', (req, res) => {
   massive = [g_league_users, s_league_users, b_league_users]
   res.json({ massive });
